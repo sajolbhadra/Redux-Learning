a792444ce2d630982d1200486ffc3c50fe017718
Niloy-Dey <niloydeyce@gmail.com>
2022-07-18 23:29:11 +0600

    tailwind css install done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import { Routes, Route, Link } from "react-router-dom";
+// import { Routes, Route, Link } from "react-router-dom";
+import { Route, Routes } from "react-router";
+import Home from "./Component/Home/Home";
+import Footer from "./Shared/Footer/Footer";
+import Navbar from "./Shared/Navbar/Navbar";
+import NotFound from "./Shared/NotFound/NotFound";
 
 function App() {
   return (
-    <div className="App">
+    <div >
+      <Navbar></Navbar>
        <Routes>
-        <Route path="/" />
-        <Route path="/" />
-        <Route path="/" />
-        <Route path="/" />
-        <Route path="about"/>
+        <Route path="/" element={<Home></Home>} />
+        <Route path="/home" element={<Home></Home>} />
+
+
+
+        <Route path="*" element={<NotFound></NotFound>} />
       </Routes>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/Component/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../../Shared/Navbar/Navbar';
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className='text-orange-500'> hello home </h1>
+        </div>
+    );
+};
+
+export default Home;
