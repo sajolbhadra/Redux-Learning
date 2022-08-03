2554c34643ed59250f911077c3ab1116b9e59a4c
MdZillurRahman <mzisanr@gmail.com>
2022-08-04 01:21:27 +0600

    Some Changes

--- src/Component/Authentication/Login.js
+++ src/Component/Authentication/Login.js
@@ -33,61 +33,69 @@ const Login = () => {
   return (
     <div className="min-h-screen bg-blue-100 pt-28 px-3">
       {/* <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100"> */}
-        <div className="flex justify-center">
-          <div className="hidden lg:block">
-            <img className="w-[400px]" src={login} alt="" />
-          </div>
-          <div className="w-full lg:w-96 bg-white px-2 py-2 lg:px-10 lg:py-8">
-            <h1 className="text-center text-2xl text-blue-500 font-bold ">Login Account</h1>
-            <form onSubmit={handleSignIn}>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input
-                  name="email"
-                  id="userEmail"
-                  type="email"
-                  placeholder="Enter your Email"
-                  className="input input-bordered"
-                />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Password</span>
-                </label>
-                <input
-                  name="password"
-                  type="password"
-                  placeholder="Enter your Password"
-                  className="input input-bordered"
-                />
-                <label className="label">
-                  <button
-                    onClick={resetPassword}
-                    className="text-red-500 label-text-alt link link-hover"
-                  >
-                    Forgot password?
-                  </button>
-
-                </label>
-              </div>
-              <div className="form-control mt-6">
-                <button type="submit" className="hover:bg-blue-500 py-3 rounded-xl bg-gray-500">LOGIN</button>
-              </div>
-              <div className="my-3">
-                <p>New Here? <Link to="/signUp" className="text-blue-600 hover:underline">
-                    Join Now
-                  </Link></p>
-              
-              </div>
-            </form>
-
-            {/* social login process start here  */}
-            <div class="divider text-blue-600 text-lg">or</div>
-            <SocialLogin></SocialLogin>
-          </div>
+      <div className="flex justify-center">
+        <div className="hidden lg:block">
+          <img className="w-[400px]" src={login} alt="" />
         </div>
+        <div className="w-full lg:w-96 bg-white px-2 py-2 lg:px-10 lg:py-8">
+          <h1 className="text-center text-2xl text-blue-500 font-bold ">
+            Login Account
+          </h1>
+          <form onSubmit={handleSignIn}>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                name="email"
+                id="userEmail"
+                type="email"
+                placeholder="Enter your Email"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                name="password"
+                type="password"
+                placeholder="Enter your Password"
+                className="input input-bordered"
+              />
+              <label className="label">
+                <button
+                  onClick={resetPassword}
+                  className="text-red-500 label-text-alt link link-hover"
+                >
+                  Forgot password?
+                </button>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button
+                type="submit"
+                className="hover:bg-blue-500 py-3 rounded-xl bg-gray-500"
+              >
+                LOGIN
+              </button>
+            </div>
+            <div className="my-3">
+              <p>
+                New Here?{" "}
+                <Link to="/signUp" className="text-blue-600 hover:underline">
+                  Join Now
+                </Link>
+              </p>
+            </div>
+          </form>
+
+          {/* social login process start here  */}
+          <div class="divider text-blue-600 text-lg">or</div>
+          <SocialLogin></SocialLogin>
+        </div>
+      </div>
       {/* </div> */}
     </div>
   );
