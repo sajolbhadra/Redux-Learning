314c83348caef88a09ad905dc021f2b74ff17eb9
MdZillurRahman <mzisanr@gmail.com>
2022-08-04 00:55:54 +0600

    Quiz

--- src/Shared/SocialLogin/SocialLogin.js
+++ src/Shared/SocialLogin/SocialLogin.js
@@ -2,21 +2,29 @@ import React from 'react';
 import { useSignInWithGithub, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import { FaFacebook, FaGithub, FaGoogle } from 'react-icons/fa';
 import auth from '../../firebase/firebase.init';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const SocialLogin = () => {
     const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
     const [signInWithGithub, gitUser, gitLoading, gitError] = useSignInWithGithub(auth);
-    const handleGoogleLogin = () => {
-        signInWithGoogle();
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    
+    let from = location.state?.from?.pathname || '/';
+
+    if(user || gitUser){
+        navigate(from, {replace: true});
     }
-    const handlGithubLogin = () => {
-        signInWithGithub();
+
+    if(user || gitUser){
+        navigate('/home');
     }
     return (
         <div className="form-control grid grid-cols-2 ">
-            <button onClick={handleGoogleLogin} className="m-1  text-xl  btn bg-base-100 text-black hover:text-white"> <FaGoogle /> </button>
+            <button onClick={() => signInWithGoogle()} className="m-1  text-xl  btn bg-base-100 text-black hover:text-white"> <FaGoogle /> </button>
             {/* <button className="m-1 text-xl  btn bg-base-100 text-black hover:text-white"> <FaFacebook /> </button> */}
-            <button onClick={handlGithubLogin} className="m-1 text-xl  btn bg-base-100 text-black hover:text-white"> <FaGithub /> </button>
+            <button onClick={() => signInWithGithub()} className="m-1 text-xl  btn bg-base-100 text-black hover:text-white"> <FaGithub /> </button>
         </div>
     );
 };
