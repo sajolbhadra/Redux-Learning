8332487684383b88e98ff75deaacb99aa7c826c6
mahiuddinhabib <mh.habib137@gmail.com>
2022-08-04 10:58:58 +0600

    quick start and index is finished

--- src/Component/Tutorial/QuickStart.js
+++ src/Component/Tutorial/QuickStart.js
@@ -14,11 +14,17 @@ const QuickStart = () => {
     };
     const pageIndex =
         <>
-            <p className='px-2 py-1'><a href="#redux-tutorial">Redux Official Tutorials</a></p>
-            <p className='px-2 py-1'><a href="#redux-tutorial">Redux Official Tutorials</a></p>
-            <p className='px-2 py-1'><a href="#redux-tutorial">Redux Official Tutorials</a></p>
-            <p className='px-2 py-1'><a href="#redux-tutorial">Redux Official Tutorials</a></p>
-            <p className='px-2 py-1'><a href="#redux-tutorial">Redux Official Tutorials</a></p>
+            <p className='px-2 py-1'><a href="#introduction">Introduction</a></p>
+            <p className='px-2 py-1'><a href="#how-to-read">How to read this tutoral</a></p>
+            <p className='px-2 py-1'><a href="#install-toolkit">Install Redux Toolkit and React-Redux</a></p>
+            <p className='px-2 py-1'><a href="#create-store">Create a Redux Store</a></p>
+            <p className='px-2 py-1'><a href="#provide-store">Provide the Redux Store to React</a></p>
+            <p className='px-2 py-1'><a href="#create-state-slice">Create a Redux State Slice</a></p>
+            <p className='px-2 py-1'><a href="#add-slice-reducer">Add Slice Reducers to the Store</a></p>
+            <p className='px-2 py-1'><a href="#UseStateandActions">Use Redux State and Actions in React Components</a></p>
+            <p className='px-2 py-1'><a href="#WhatLearned">What You've Learned</a></p>
+            <p className='px-2 py-1'><a href="#FullCounterAppExample">Full Counter App Example</a></p>
+            <p className='px-2 py-1'><a href="#WhatNext?">What's Next?</a></p>
         </>
 
     return (
@@ -62,12 +68,12 @@ const QuickStart = () => {
                         </ul>
                     </div>
 
-                    <h2 className='text-4xl font-bold mb-5' id='redux-tutorial'>Introduction</h2>
+                    <h2 id='introduction' className='text-4xl font-bold mb-5' id='redux-tutorial'>Introduction</h2>
                     <p className='mb-5'>
                         Welcome to the Redux Toolkit Quick Start tutorial! <span className='font-bold'> This tutorial will briefly introduce you to Redux Toolkit and teach you how to start using it correctly.</span>
                     </p>
 
-                    <h3 className='text-3xl font-bold mb-5'>How to Read This Tutorial</h3>
+                    <h3 id='how-to-read' className='text-3xl font-bold mb-5'>How to Read This Tutorial</h3>
                     <p className='mb-5'>
                         This page will focus on just how to set up a Redux application with Redux Toolkit and the main APIs you'll use. For explanations of what Redux is, how it works, and full examples of how to use Redux Toolkit,<Link to={'/tutorial'} className='text-[#764abc] hover:underline'> see the tutorials linked in the "Tutorials Index" page.</Link>
                     </p>
@@ -79,7 +85,7 @@ const QuickStart = () => {
                     </p>
 
                     <h2 className='text-4xl font-bold mb-5' id='redux-tutorial'>Usage Summary</h2>
-                    <h3 className='text-3xl font-bold mb-5'>Install Redux Toolkit and React-Redux</h3>
+                    <h3 id='install-toolkit' className='text-3xl font-bold mb-5'>Install Redux Toolkit and React-Redux</h3>
                     <p className='mb-5'>
                         Add the Redux Toolkit and React-Redux packages to your project:
                     </p>
@@ -96,7 +102,7 @@ const QuickStart = () => {
                         </p>
                     </div>
 
-                    <h3 className='text-3xl font-bold mb-5'>Create a Redux Store</h3>
+                    <h3 id='create-store' className='text-3xl font-bold mb-5'>Create a Redux Store</h3>
                     <p className='mb-5'>
                         Create a file named <span className='bg-gray-300 px-2 pb-[3px] border border-gray-500 rounded'>src/app/store.js.</span> Import the <span className='bg-gray-300 px-2 pb-[3px] border border-gray-500 rounded'>configureStore</span> API from Redux Toolkit. We'll start by creating an empty Redux store, and exporting it:
                     </p>
@@ -121,7 +127,7 @@ const QuickStart = () => {
                         This creates a Redux store, and also automatically configure the Redux DevTools extension so that you can inspect the store while developing.
                     </p>
 
-                    <h3 className='text-3xl font-bold mb-5'>Provide the Redux Store to React</h3>
+                    <h3 id='provide-store' className='text-3xl font-bold mb-5'>Provide the Redux Store to React</h3>
                     <p className='mb-5'>
                         Once the store is created, we can make it available to our React components by putting a React-Redux<span className='bg-gray-300 px-2 pb-[3px] border border-gray-500 rounded'>&#60;Provider&#62;</span>  around our application in <span className='bg-gray-300 px-2 pb-[3px] border border-gray-500 rounded'>src/index.js.</span> Import the Redux store we just created, put a <span className='bg-gray-300 px-2 pb-[3px] border border-gray-500 rounded'>&#60;Provider&#62;</span> around your <span className='bg-gray-300 px-2 pb-[3px] border border-gray-500 rounded'>&#60;App&#62;</span>, and pass the store as a prop:
                     </p>
@@ -142,7 +148,7 @@ const QuickStart = () => {
                             &#125;&#41;
                         </p>
                     </div>
-                    <h3 className='text-3xl font-bold mb-5'>Create a Redux State Slice</h3>
+                    <h3 id='create-state-slice' className='text-3xl font-bold mb-5'>Create a Redux State Slice</h3>
                     <p className='mb-5'>
                         Add a new file named <span className='bg-gray-300 px-2 pb-[3px] border border-gray-500 rounded'>src/features/counter/counterSlice.js</span>. In that file, import the <span className='bg-gray-300 px-2 pb-[3px] border border-gray-500 rounded'>createSlice</span> API from Redux Toolkit.
                     </p>
@@ -170,7 +176,7 @@ const QuickStart = () => {
                         </p>
                     </div>
 
-                    <h3 className='text-3xl font-bold mb-5'>Add Slice Reducers to the Store</h3>
+                    <h3 id='add-slice-reducer' className='text-3xl font-bold mb-5'>Add Slice Reducers to the Store</h3>
                     <p className='mb-5'>
                         Next, we need to import the <span className='bg-gray-300 px-2 pb-[3px] border border-gray-500 rounded'>reducer</span> function from the counter slice and add it to our store. By defining a field inside the reducer parameter, we tell the store to use this slice reducer function to handle all updates to that state.
                     </p>
@@ -192,7 +198,7 @@ const QuickStart = () => {
                         </p>
                     </div>
 
-                    <h3 className='text-3xl font-bold mb-5'>Use Redux State and Actions in React Components</h3>
+                    <h3 id='UseStateandActions' className='text-3xl font-bold mb-5'>Use Redux State and Actions in React Components</h3>
                     <p className='mb-5'>
                         Now we can use the React-Redux hooks to let React components interact with the Redux store. We can read data from the store with <span className='bg-gray-300 px-2 pb-[3px] border border-gray-500 rounded'>useSelector</span>, and dispatch actions using <span className='bg-gray-300 px-2 pb-[3px] border border-gray-500 rounded'>useDispatch</span>. Create a <span className='bg-gray-300 px-2 pb-[3px] border border-gray-500 rounded'>src/features/counter/Counter.js</span> file with a <span className='bg-gray-300 px-2 pb-[3px] border border-gray-500 rounded'>&#60;Counter&#62;</span> component inside, then import that component into <span className='bg-gray-300 px-2 pb-[3px] border border-gray-500 rounded'>App.js</span> and render it inside of  <span className='bg-gray-300 px-2 pb-[3px] border border-gray-500 rounded'>&#60;App&#62;</span>.
                         </p>
@@ -223,7 +229,7 @@ const QuickStart = () => {
                                 <li>The  <span className='bg-gray-300 px-2 pb-[3px] border border-gray-500 rounded'>&#60;Counter&#62;</span> component will see the new state value from the store and re-render itself with the new data</li>
                             </ul>
 
-                            <h3 className='text-3xl font-bold mb-5'>What You've Learned</h3>
+                            <h3 id='WhatLearned' className='text-3xl font-bold mb-5'>What You've Learned</h3>
                             <p className='mb-5'>
                                 That was a brief overview of how to set up and use Redux Toolkit with React. Recapping the details:
                             </p>
@@ -234,7 +240,7 @@ const QuickStart = () => {
                                 </ul>
                             </div>
 
-                            <h3 className='text-3xl font-bold mb-5'>Full Counter App Example</h3>
+                            <h3 id='FullCounterAppExample' className='text-3xl font-bold mb-5'>Full Counter App Example</h3>
                             <p className='mb-5'>
                                 Here's the complete counter application as a running CodeSandbox:
                             </p>
@@ -248,14 +254,14 @@ const QuickStart = () => {
                                 sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
                             ></iframe>
 
-                            <h3 className='text-3xl font-bold mb-5'>What's Next?</h3>
+                            <h3 id='WhatNext' className='text-3xl font-bold mb-5'>What's Next?</h3>
                             <p className='mb-5'>
                             We recommend going through <Link to={'/tutorial'} className='text-[#764abc] hover:underline'>the "Redux Essentials" and "Redux Fundamentals" tutorials in the Redux core docs</Link>, which will give you a complete understanding of how Redux works, what Redux Toolkit does, and how to use it correctly.
                             </p>
                         </div>
 
                         {/* right side index bar for desktop */}
-                        <div className='col-span-2 relative hidden lg:block'>
+                        <div className='col-span-2 relative hidden lg:block z-0'>
                             <div className='fixed top-[100px] border-l-2 border-gray-300 p-4'>
                                 {pageIndex}
                             </div>
