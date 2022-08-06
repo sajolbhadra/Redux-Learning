1185c2a40285a9352f834f6adc7d4b67702fe812
Niloy-Dey <niloydeyce@gmail.com>
2022-08-06 23:00:23 +0600

    theme work done

---
+++ src/Component/Dashboard/User/Analysis.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Analysis = () => {
+    return (
+        <div className='pt-16 '>
+            <p>Analysis</p>
+        </div>
+    );
+};
+
+export default Analysis;
