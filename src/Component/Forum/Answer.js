8d855c2f121cad4610dc169a3e67104749b8d16e
mahiuddinhabib <mh.habib137@gmail.com>
2022-08-18 18:01:41 +0600

    basic structure formed

--- src/Component/Forum/Answer.js
+++ src/Component/Forum/Answer.js
@@ -4,7 +4,7 @@ const Answer = ({answer}) => {
     const {name, date, ans} = answer;
     // console.log(name, date, ans);
     return (
-        <div>
+        <div className='bg-blue-200 mb-3'>
             <h3>{name}</h3>
             <p>{date}</p>
             <p>{ans}</p>
--- src/Component/Forum/Discussion.js
+++ src/Component/Forum/Discussion.js
@@ -2,20 +2,26 @@ import React from 'react';
 import Answer from './Answer';
 
 const Discussion = ({ discussion }) => {
-    const { name, picture, date, question, answers } = discussion;
+    const { name, picture, date, question } = discussion;
 
-    const noOfAns = answers.length;
+    let noOfAns;
+    discussion?.answers ? noOfAns = discussion?.answers.length : noOfAns=0;
+    
     return (
-        <div className='mb-6'>
+        <div className='mb-6 bg-fuchsia-300'>
             <h2>{name}</h2>
             <p>{date}</p>
             <p>{noOfAns}</p>
             {
-                answers.map((answer, index) => <Answer
+             noOfAns? discussion?.answers.map((answer, index) => <Answer
                     key={index}
                     answer={answer}
-                ></Answer>)
+                ></Answer>):''
             }
+            <div class="form-control">
+                <textarea class="textarea textarea-bordered h-24" placeholder="Answer This Question"></textarea>
+                <button class="btn btn-outline">post</button>
+            </div>
         </div>
     );
 };
