658398030f638b390120a04b3c78ea6cf7abc39c
mahiuddinhabib <mh.habib137@gmail.com>
2022-08-19 14:39:32 +0600

    input answer style fixed

--- src/Component/Forum/Answer.js
+++ src/Component/Forum/Answer.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
-const Answer = ({answer}) => {
-    const {name, date, ans} = answer;
+const Answer = ({ answer }) => {
+    const { name, date, ans, picture } = answer;
     // console.log(name, date, ans);
     return (
-        <div className='bg-blue-200 mb-3'>
-            <h3>{name}</h3>
-            <p>{date}</p>
+        <div className='bg-blue-200 mt-3 p-4'>
+            <div className='flex items-center'>
+                <img className='w-[35px] h-[35px] rounded-full inline' src={picture} alt="" />
+                <div className='ml-3'>
+                    <h3>{name}</h3>
+                    <p className=' text-xs'>{date}</p>
+                </div>
+            </div>
             <p>{ans}</p>
         </div>
     );
--- src/Component/Forum/Discussion.js
+++ src/Component/Forum/Discussion.js
@@ -19,15 +19,15 @@ const Discussion = ({ discussion }) => {
                 <div className='col-span-10 lg:col-span-2 pt-8 pl-4 bg-slate-300'>
                     <img className='w-[70px] rounded-full absolute top-[-35px]' src={picture} alt="" />
                     <h2>{name}</h2>
-                    <p>{date}</p>
+                    <p className='text-sm'>{date}</p>
                 </div>
-                <div className='col-span-10 lg:col-span-8 p-4 bg-green-200'>
+                <div className='col-span-10 lg:col-span-8 p-4 pb-0 bg-green-200'>
                     {question}
+                    <p className='mt-4 mb-2 text-right'>{noOfAns} answer(s)</p>
                 </div>
             </div>
 
 
-            <p>{noOfAns}</p>
             <div style={{ display: isAnsOpen ? 'block' : 'none' }}>
                 {
                     noOfAns ? discussion?.answers.map((answer, index) => <Answer
@@ -36,11 +36,11 @@ const Discussion = ({ discussion }) => {
                     ></Answer>) : ''
                 }
             </div>
-            <button style={{ display: noOfAns ? 'inline' : 'none' }} onClick={() => { setIsAnsOpen(!isAnsOpen) }}>{isAnsOpen ? 'Hide Answers' : 'Show Answers'}</button>
-            <button onClick={() => { setIsQueOpen(!isQueOpen) }}>Answer This Question</button>
-            <div class="form-control" style={{ display: isQueOpen ? 'block' : 'none' }}>
+            <button className='mx-4 my-1' style={{ display: noOfAns ? 'inline' : 'none' }} onClick={() => { setIsAnsOpen(!isAnsOpen) }}>{isAnsOpen ? 'Hide Answers' : 'Show Answers'}</button>
+            <button className='mx-4 my-1' onClick={() => { setIsQueOpen(!isQueOpen) }}>Answer This Question</button>
+            <div class="form-control w-full md:w-2/5 p-4" style={{ display: isQueOpen ? 'flex' : 'none' }}>
                 <textarea class="textarea textarea-bordered h-24" placeholder="Answer This Question"></textarea>
-                <button onClick={handlePostQuestion} class="btn btn-outline">post</button>
+                <button onClick={handlePostQuestion} class="btn btn-sm w-[70px] mt-4">post</button>
             </div>
         </div>
     );
