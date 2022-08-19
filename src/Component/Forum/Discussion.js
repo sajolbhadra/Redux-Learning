6bc85bdde308b89d8096f5eb5fafbe14ae6d7d4a
mahiuddinhabib <mh.habib137@gmail.com>
2022-08-19 12:22:39 +0600

    positioning and displays are fixed

--- src/Component/Forum/Discussion.js
+++ src/Component/Forum/Discussion.js
@@ -8,28 +8,39 @@ const Discussion = ({ discussion }) => {
     const [isQueOpen, setIsQueOpen] = useState(false);
 
     let noOfAns;
-    discussion?.answers ? noOfAns = discussion?.answers.length : noOfAns=0;
-    
+    discussion?.answers ? noOfAns = discussion?.answers.length : noOfAns = 0;
+
+    const handlePostQuestion = () => {
+        setIsQueOpen(!isQueOpen);
+    }
     return (
-        <div className='mb-6 bg-fuchsia-300'>
-            <h2>{name}</h2>
-            <p>{date}</p>
-            <p>{noOfAns}</p>
-            
-            
-            <div style={{ display: isAnsOpen? 'block': 'none'}}>
-            {
-             noOfAns? discussion?.answers.map((answer, index) => <Answer   
-                    key={index}
-                    answer={answer}
-                ></Answer>):''
-            }
+        <div className='relative my-12 mx-4 bg-fuchsia-300'>
+            <div className='grid grid-flow-row-dense grid-cols-10 '>
+                <div className='col-span-10 lg:col-span-2 pt-8 pl-4 bg-slate-300'>
+                    <img className='w-[70px] rounded-full absolute top-[-35px]' src={picture} alt="" />
+                    <h2>{name}</h2>
+                    <p>{date}</p>
+                </div>
+                <div className='col-span-10 lg:col-span-8 p-4 bg-green-200'>
+                    {question}
+                </div>
             </div>
-            <button style={{ display: noOfAns? 'inline': 'none'}} onClick={()=>{setIsAnsOpen(!isAnsOpen)}}>{isAnsOpen?'Hide Answers':'Show Answers'}</button>
-            <button onClick={()=>{setIsQueOpen(!isQueOpen)}}>Answer This Question</button>
-            <div class="form-control"  style={{ display: isQueOpen? 'block': 'none'}}>
+
+
+            <p>{noOfAns}</p>
+            <div style={{ display: isAnsOpen ? 'block' : 'none' }}>
+                {
+                    noOfAns ? discussion?.answers.map((answer, index) => <Answer
+                        key={index}
+                        answer={answer}
+                    ></Answer>) : ''
+                }
+            </div>
+            <button style={{ display: noOfAns ? 'inline' : 'none' }} onClick={() => { setIsAnsOpen(!isAnsOpen) }}>{isAnsOpen ? 'Hide Answers' : 'Show Answers'}</button>
+            <button onClick={() => { setIsQueOpen(!isQueOpen) }}>Answer This Question</button>
+            <div class="form-control" style={{ display: isQueOpen ? 'block' : 'none' }}>
                 <textarea class="textarea textarea-bordered h-24" placeholder="Answer This Question"></textarea>
-                <button class="btn btn-outline">post</button>
+                <button onClick={handlePostQuestion} class="btn btn-outline">post</button>
             </div>
         </div>
     );
--- src/Component/Forum/Forum.js
+++ src/Component/Forum/Forum.js
@@ -9,13 +9,21 @@ const Forum = () => {
             .then(data => setDiscussions(data))
     }, [])
     return (
-        <div className='my-20'>
-            {
-                discussions.map((discussion, index) => <Discussion
-                    key={index}
-                    discussion={discussion}
-                ></Discussion>)
-            }
+        <div className='grid grid-flow-row-dense grid-cols-10 my-20'>
+            <div className='col-span-10 lg:col-span-8'>
+                {
+                    discussions.map((discussion, index) => <Discussion
+                        key={index}
+                        discussion={discussion}
+                    ></Discussion>)
+                }
+            </div>
+            <div className='relative col-span-10 lg:col-span-2 order-first md:order-last bg-orange-200'>
+                <div className='md:fixed top-[80px]'>
+                    <h2>Have You any Question?</h2>
+                    <h3>Ask your Question</h3>
+                </div>
+            </div>
         </div>
     );
 };
