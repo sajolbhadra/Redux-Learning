8108ae18dedddf7cdc34c28b4e543af5747b8bf8
mahiuddinhabib <mh.habib137@gmail.com>
2022-08-30 09:26:41 +0600

    margin padding fixed

--- src/Shared/Translate/GoogleTranslate.js
+++ src/Shared/Translate/GoogleTranslate.js
@@ -7,8 +7,11 @@ const GoogleTranslate = () => {
     const [transOpen, setTransOpen]=useState(false);
     return (
             <div className='flex flex-row items-center'>
-                <div id="google_translate_element" className={`md: mt-[14px] ${transOpen? 'block':'hidden'}`}></div>
-                <button className='text-[28px] w-[30px] h-[30px] border rounded-full mx-3' onClick={()=>setTransOpen(!transOpen)}><TbLanguage></TbLanguage></button>
+                <div id="google_translate_element" className={`md: mt-[14px] mr-3 ${transOpen? 'block':'hidden'}`}></div>
+                <button className='text-[28px] w-[30px] h-[30px] border rounded-full mr-3' 
+                        onClick={()=>setTransOpen(!transOpen)}
+                        style={{ boxShadow: transOpen ? "2px 2px 9px 0.1px #B3C5EF" : "" }}
+                        ><TbLanguage></TbLanguage></button>
             </div>
     );
 };
