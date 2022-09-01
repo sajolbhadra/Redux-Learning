8707f9396b15ffb75c4fe621c16ba4f34fcb31b3
abusayeed4bd <abusayeed4bd@gmail.com>
2022-09-01 15:04:46 +0600

    experience error

--- src/Component/Dashboard/Profile/ExperienceUpdate.js
+++ src/Component/Dashboard/Profile/ExperienceUpdate.js
@@ -1,9 +1,45 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from './../../../firebase/firebase.init';
+import { toast } from 'react-toastify';
 
 const ExperienceUpdate = () => {
+    const [user] = useAuthState(auth)
+    const email = user?.email;
+
     const handleEducationUpdate = e => {
         e.preventDefault();
+        const experience1 = e.target.experience1.value;
+        const description1 = e.target.description1.value;
+        const time1 = e.target.time1.value;
+        const experience2 = e.target.experience2.value;
+        const description2 = e.target.description2.value;
+        const time2 = e.target.time2.value;
+        const experience3 = e.target.experience3.value;
+        const description3 = e.target.description3.value;
+        const time3 = e.target.time3.value;
+        const data = { experience1, description1, time1, experience2, description2, time2, experience3, description3, time3 };
 
+        fetch(`http://localhost:5000/experience/${email}`, {
+            method: "PUT",
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringify(data),
+        })
+            .then((res) => res.json())
+            .then((result) => {
+                if (result.upsertedCount === 1) {
+                    toast("Updated!!");
+
+                } else {
+                    toast.error("Already Updated!!");
+                }
+            });
+
+
+
+        console.log(data);
 
     }
     return (
